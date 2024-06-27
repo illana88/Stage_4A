b851eca35ba7b39ffc2d77f08f353aaca361700b
illana88 <illanarabasquinho@gmail.com>
2024-06-27 15:05:36 +0200

    test 27/06

--- Test_pgp-a.py
+++ Test_pgp-a.py
@@ -1,4 +1,5 @@
 import os
+import glob
 
 arg1 = int(input("Argument 1 :"))
 if os.access("Summary_stats.txt",os.F_OK):
@@ -7,13 +8,13 @@ if os.access("Summary_stats.txt",os.F_OK):
 if arg1==0 or arg1==2 or arg1==5 :
     with open("Summary_stats.txt","w") as fichier :
         fichier.write("STARTING pgp-a.sh WITH FLAG 0\n")
-    phrase = "STARTED StringTie Calculation ... of samples\n"
+    phrase = "STARTED SringTie Calculations - Will take a while depending on number of samples\n"
     print(phrase)
     with open("Summary_stats.txt","a") as fichier :
         fichier.write(phrase)
-#     if len(os.listdir("iPSC_gtfs"))==0 :
-#         for f in glob.glob("*.*") :
-#             os.remove(f)
-#         os.makedirs("iPSC_gtfs",exist_ok=True)
+    if len(os.listdir("iPSC_gtfs"))!=0 :
+        for f in glob.glob("*.*") :
+            os.remove(f)
+        os.makedirs("iPSC_gtfs",exist_ok=True)
 #     file = open("all_bams.tsv")
 #     reader = csv.reader(file,delimiter='\t')
