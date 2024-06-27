cbfcb7a61078403fc963937461a72e6dcc07872c
illana88 <157378556+illana88@users.noreply.github.com>
2024-06-27 10:26:12 +0200

    Add files via upload

---
+++ Test_pgp-a.py
@@ -0,0 +1,19 @@
+import os
+
+arg1 = int(input("Argument 1 :"))
+# if os.access("Summary_stats",os.F_OK):
+#     os.remove(("Summary_stats"))
+
+# if arg1==0 or arg1==2 or arg1==5 :
+#     with open("Summary_stats.txt","w+") as fichier :
+#         fichier.write("STARTING pgp-a.sh WITH FLAG 0")
+#     phrase = "STARTED StringTie Calculation ... of samples"
+#     print(phrase)
+#     with open("Summary_stats.txt","w+") as fichier :
+#         fichier.write(phrase)
+#     if len(os.listdir("iPSC_gtfs"))==0 :
+#         for f in glob.glob("*.*") :
+#             os.remove(f)
+#         os.makedirs("iPSC_gtfs",exist_ok=True)
+#     file = open("all_bams.tsv")
+#     reader = csv.reader(file,delimiter='\t')
