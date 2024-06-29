6a76a6e31d7089197ef9b570128f006d805798e1
illana88 <illanarabasquinho@gmail.com>
2024-06-29 15:45:59 +0200

    test 29/06

--- Test_pgp-a.py
+++ Test_pgp-a.py
@@ -39,7 +39,8 @@ if arg1==0 or arg1==2 or arg1==5 :
             print("STARTED PROCESSING SAMPLE $sample")
             with open("Summary_stats.txt","a") as fichier :
                 fichier.write("STARTED PROCESSING SAMPLE $sample\n")
-            samp = os.path.splitext(os.path.basename(sample))[0]
+            samp = os.path.splitext(os.path.basename(sample))[0] ## pb : pas la bon samp
+            print(samp)
             commande_shell = f"stringtie {sample} -p 8 -G gencode.v38.annotation.gtf -o iPSC_gtfs/{samp}.gtf"
             subprocess.run(commande_shell, shell=True, check=True)
         if len(os.listdir("iPSC_gtfs"))!=0 :
