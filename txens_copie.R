d56f23a4f68e401b9e5a268bcbfb21b8faf3947a
illana88 <157378556+illana88@users.noreply.github.com>
2024-09-03 09:20:57 +0200

    Delete txens_copie.R

--- txens_copie.R
+++
@@ -1,8 +0,0 @@
-library(AnnotationHub)
-library(ensembldb)
-
-ah <- AnnotationHub()
-edb=query(ah, c("EnsDb", "Hsapiens", "105"))[[1]]
-
-tx_lens=transcriptLengths(edb,with.utr5_len = TRUE,with.utr3_len = TRUE)
-head(tx_lens)
